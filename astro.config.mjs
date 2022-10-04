b9995b844933eb0f52981ecd4d835686cf9862c1
krisztian.maurer <krisztian.maurer@metrix.co.hu>
2022-10-04 14:44:36 +0200

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact()],
+  site: 'https://maurerkrisztian.com',
+  integrations: [preact(), sitemap()]
 });
